442237cc4310c541317a30bd9574d84150cbe6ea
jmslynn <72352820+jmslynn@users.noreply.github.com>
2021-09-08 18:16:44 -0400

    added starter data and list of functions needed

---
+++ npcs.js
@@ -0,0 +1,41 @@
+//to hold the data
+const npcs = [
+  {
+  "_id": 1,
+"name": "Lydia Bennet",
+"plot": "will tell you where the soldiers are stationed"
+},
+{  "_id": 2,
+"name": "Lady Catherine de Bourgh",
+"plot": "will condescend to tell you how to better arrange your pack"
+},
+{  "_id": 3,
+"name": "Jennifer Strange",
+"plot": "will tell you about quark beasts"
+}
+]
+
+
+function addNpc() {
+
+}
+
+function editNpc() {
+
+}
+
+function showAllNpcs() {
+
+}
+
+function showANpc() {
+
+}
+
+function deleteANpc() {
+
+}
+
+function calculateId() {
+  // equals npcs.length + 2?
+}
